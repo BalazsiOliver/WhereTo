9b48cd4cc2bb2d868e96a0b236c1a31840a924df
Balázsi Olivér <b.oliver.muka@gmail.com>
2025-10-31 15:13:09 +0100

    Adding secret key to jwt token(otw)

--- src/main/java/com/DragonFart/WhereTo/ChatPart/service/JWTService.java
+++ src/main/java/com/DragonFart/WhereTo/ChatPart/service/JWTService.java
@@ -1,7 +1,35 @@
 package com.DragonFart.WhereTo.ChatPart.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JWTService {
-    public String generateToken() {
-        return "";
+
+    private String secretKey = "cF781A";
+    public String generateToken(String username) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
+                .and()
+                .signWith(getKey())
+                .compact();
     }
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor( secretKey);
+    }
+
+
+    // 2:31:52 video time
 }
