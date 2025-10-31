51869ddee4e0cff6f457aa47884a104f0c907599
Balázsi Olivér <b.oliver.muka@gmail.com>
2025-10-31 09:32:23 +0100

    Implementing JWT token dependencies

---
+++ src/main/java/com/DragonFart/WhereTo/ChatPart/service/JWTService.java
@@ -0,0 +1,7 @@
+package com.DragonFart.WhereTo.ChatPart.service;
+
+public class JWTService {
+    public String generateToken() {
+        return "";
+    }
+}
--- src/main/java/com/DragonFart/WhereTo/ChatPart/service/UserService.java
+++ src/main/java/com/DragonFart/WhereTo/ChatPart/service/UserService.java
@@ -3,11 +3,20 @@ package com.DragonFart.WhereTo.ChatPart.service;
 import com.DragonFart.WhereTo.ChatPart.model.Users;
 import com.DragonFart.WhereTo.ChatPart.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserRepo repo;
@@ -18,5 +27,14 @@ public class UserService {
 
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+       if(authentication.isAuthenticated()) return jwtService.generateToken();
+
+       return "fail";
     }
 }
